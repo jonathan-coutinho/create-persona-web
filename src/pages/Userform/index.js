c2709405471b1b15bc7ad9017d7a979bffd7e7cc
jonathan-coutinho <coutinho604@gmail.com>
2019-12-05 19:29:48 -0300

    Layout página user e reescrevendo como class

--- src/pages/Userform/index.js
+++ src/pages/Userform/index.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import "./styles.css"
+import {Link} from "react-router-dom"
 
-function UserForm() {
-  return (
-    <div className="userform">
-      <h2>Página usuário</h2>
-    </div>
-  );
+class UserForm extends Component {
+ 
+    render() {
+        return (
+            <div className="myContainer">
+              <h2 className="header">Gerador de Persona</h2>
+              <h4 className="myControl">Queremos saber quem você é</h4>
+              <form className={"myForm"}>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Qual o seu nome?</label>
+             <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Joaquim Teixeira"></input>
+            <small id="emailHelp" class="form-text text-muted">Insira seu nome</small>
+          </div>
+          <div class="form-group">
+            <label for="exampleInputEmail1">Qual o seu email?</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"></input>
+            <small id="emailHelp" class="form-text text-muted">Insira seu e-mail</small>
+          </div>
+          <div class="form-group">
+            <label for="exampleFormControlSelect1">Qual seu cargo?</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+              <option>CEO/Presidente</option>
+              <option>Jornalista</option>
+              <option>Engenheiro</option>
+              <option>Estudante</option>
+              <option>Outros</option>
+            </select>
+          </div>  
+         
+        </form>
+            <footer className="mySpacingContainer">
+            <Link to ="/" type="submit" class="btn btn-primary btn-lg">Voltar</Link>
+            <button type="submit" class="btn btn-primary btn-lg">Avançar</button>
+            </footer>
+            </div>
+          );
+    }
 }
 
 export default UserForm;
