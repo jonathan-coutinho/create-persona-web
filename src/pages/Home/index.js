e72a546743f1c8caafe8c4ee4719e39940cc67e3
jonathan-coutinho <coutinho604@gmail.com>
2019-12-05 18:53:43 -0300

    importando bootstrap e linkando páginas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import "./styles.css"
+import logo from "./assets/logo.jpeg"
+
+import {Link} from "react-router-dom"
 
 function Home() {
   return (
     <div className="home">
-      <h2>Home page</h2>
+      <h2>Gerador de Personas</h2>
+      <img alt = "foto principal" src={logo}></img>
+      <h4>Empresas procuram diretamente um criador de Personas</h4>
+      <h5>Faça logo isso para não ficar para trás</h5>
+      <Link to="User" className="btn btn-primary">Começar</Link>
     </div>
   );
 }
